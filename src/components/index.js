b4bc9f92f14a082ff561c39fcb004efdc0199aed
luanzixuan <yimuzx@foxmail.com>
2021-11-10 17:20:53 +0800

    打包测试

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
+import Vue from 'vue'
+import LemonIMUI from 'lemon-imui';
+import 'lemon-imui/dist/index.css';
+Vue.use(LemonIMUI);
 import YMImBase from './news.vue';
+
 YMImBase.install = function(Vue) {
     Vue.component(YMImBase.name, YMImBase);
 };
