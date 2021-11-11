05224093829edd801ae1b7ceaa9a6cceaa2041d8
luanzixuan <yimuzx@foxmail.com>
2021-11-11 11:57:58 +0800

    ceshi

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,62 @@
+const path = require('path');
+function resolve(dir) {
+    return path.resolve(__dirname, dir)
+}
+var webpack = require('webpack')
 module.exports = {
-  publicPath: "./", // 公共路径 默认为"/"，建议使用"./"相对路径
-  devServer: {   // 本地服务器配置(npm run serve)
-    port: 8080, // 端口
-    host: "localhost", // 域名
-    https: false, // 是否开启https
-    open: true	// 是否在开启服务器后自动打开浏览器访问该服务器
-  },
-  lintOnSave: false,  // 取消lint语法检测，此处可不配置
-  outputDir:"dist", // build打包输出目录
-  assetsDir:"assets", // 静态文件输出目录，基于dist
-  indexPath: "index.html",  // 输出html文件名
-  productionSourceMap: false, // 取消.map文件的打包，加快打包速度
-  configureWebpack: (config) => {
-    // process.env为环境变量，分别对应.env.development文件和.env.production文件 此处表示加快开发环境打包速度
-    if (process.env.NODE_ENV !== 'production') return;
-    config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;	//生产环境去掉console.log
-    return {  // 此处配置webpack.config.js的相关配置
-      plugins: [],
-      performance: {}
-    };
-  }
-};
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        },
+    },
+    
+
+    productionSourceMap: false,
+    configureWebpack: {
+        output: {
+            libraryExport: 'default'
+        },
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+              '@': resolve('src'),
+              '_c': resolve('src/components'),
+            }
+        }, 
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                plupload: "plupload"
+            })
+        ],
+        module: { //这个节点用于配置所有的第三方模块加载器
+          rules: [
+              {test:/\.js$/, use:'babel-loader',exclude:/node_modules/},
+              {test: /\.vue$/, use: 'vue-loader'}
+          ]
+      }
+    },
+    devServer:{
+        port: 8091,
+        hot: true,
+        open: 'Google Chrome'
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('js')
+            .include
+            .add('/packages')
+            .end()
+            .use('babel')
+            .loader('babel-loader')
+            .tap(options => {
+                return options
+            })
+    },
+    css: {
+        extract: false,
+    }
+}
