0b3069ed4d86daabbd55c9980b28a6241372554b
Maggie <13256724140@163.com>
2021-12-02 17:49:42 +0800

    自定义组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="home">
-    <news ref="yimuNews"></news>
+    <news ref="yimuNews" :customMenu="customMenu"></news>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import News from '../components/news.vue'
+import News from '../components/news.vue';
+import testComponent from '../components/testComponent.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      customMenu: [],
+    };
+  },
   components: {
-    News
+    News,
   },
-  mounted () {
+  mounted() {
     setTimeout(() => {
+      this.customMenu = [
+        {
+          name: 'files',
+          isBottom: false,
+          title: '文档',
+          unread: 0,
+          key: 'test',
+          icon:
+            'https://img12.iqilu.com/1/sucaiku/image_thumb/202110/19/4ada1f7d8413436abfbd2ca48f7ef009.png',
+          component: testComponent,
+        },
+      ];
       console.log(this.$refs.yimuNews.im);
-    }, 100) 
+    }, 500);
   },
-  destroyed () {
+  destroyed() {
     console.log('销毁');
   },
-}
+};
 </script>
