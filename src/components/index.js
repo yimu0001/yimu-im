b4a5ef6599ad8d2bb77f7da33727216476845450
Maggie <13256724140@163.com>
2021-12-09 11:34:17 +0800

    更改了导出方式

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,18 @@
 // Vue.use(LemonIMUI);
 import YimuIm from './news.vue';
 
+import iconfont from '@/assets/iconfont/iconfont.css';
+import LemonMessageImage from '@/components/message/image.vue';
+import LemonMessageText from '@/components/message/text.vue';
+import LemonMessageFile from '@/components/message/file.vue';
+
 YimuIm.install = function(Vue) {
-    Vue.component(YimuIm.name, YimuIm);
+  Vue.component(LemonMessageImage.name, LemonMessageImage);
+  Vue.component(LemonMessageText.name, LemonMessageText);
+  Vue.component(LemonMessageFile.name, LemonMessageFile);
+
+  Vue.component(YimuIm.name, YimuIm);
 };
+
 export default YimuIm;
+export { iconfont };
--- vue.config.js
+++ vue.config.js
@@ -1,53 +1,56 @@
-var webpack = require('webpack')
+var webpack = require('webpack');
 const path = require('path');
 function resolve(dir) {
-    return path.resolve(__dirname, dir)
+  return path.resolve(__dirname, dir);
 }
-module.exports = {
-    pages: {
-        index: {
-            entry: 'src/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
-        },
-    },
-    configureWebpack: {
-        output: {
-            libraryExport: 'default'
-        },
-        resolve: {
-            extensions: ['.js', '.vue', '.json'],
-            alias: {
-              '@': resolve('src'),
-              '_c': resolve('src/components'),
-            }
-        }, 
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                plupload: "plupload"
-            })
-        ]
-    },
-    // 如果你不需要使用eslint，把lintOnSave设为false即可
-    lintOnSave: false,
-    // 打包时不生成.map文件
-    productionSourceMap: false,
-    // chainWebpack: config => {
-    //     config.module
-    //         .rule('js')
-    //         .include
-    //         .add('/packages')
-    //         .end()
-    //         .use('babel')
-    //         .loader('babel-loader')
-    //         .tap(options => {
-    //             return options
-    //         })
-    // },
-    css: {
-        extract: false,
-    },
 
-}
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/dist' : '';
+
+module.exports = {
+  publicPath: BASE_URL,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+  },
+  configureWebpack: {
+    output: {
+      libraryExport: 'default',
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': resolve('src'),
+        _c: resolve('src/components'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        plupload: 'plupload',
+      }),
+    ],
+  },
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: false,
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+  // chainWebpack: config => {
+  //     config.module
+  //         .rule('js')
+  //         .include
+  //         .add('/packages')
+  //         .end()
+  //         .use('babel')
+  //         .loader('babel-loader')
+  //         .tap(options => {
+  //             return options
+  //         })
+  // },
+  css: {
+    extract: false,
+  },
+};
