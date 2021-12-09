e5a726cb4a2818425ea52335edbee044b1ab4d25
Maggie <13256724140@163.com>
2021-12-09 18:12:00 +0800

    iconfont引入方式暂时改为阿里线上地址

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,8 @@ import YimuIm from './news.vue';
 import LemonMessageImage from './message/image.vue';
 import LemonMessageText from './message/text.vue';
 import LemonMessageFile from './message/file.vue';
-import '../assets/iconfont/iconfont.css';
+// import '../assets/iconfont/iconfont.css';
+import './style.css';
 
 YimuIm.install = function(Vue) {
   Vue.component(YimuIm.name, YimuIm);
