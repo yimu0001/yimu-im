79aa21449ed87a7571d294604803158d22d6d926
Maggie <13256724140@163.com>
2021-12-10 11:48:32 +0800

    去掉LemonMessageBasic

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import YimuIm from './news.vue';
-import LemonMessageBasic from 'lemon-imui';
 import LemonMessageImage from './message/image.vue';
 import LemonMessageText from './message/text.vue';
 import LemonMessageFile from './message/file.vue';
@@ -8,7 +7,6 @@ import './style.css';
 
 YimuIm.install = function(Vue) {
   Vue.component(YimuIm.name, YimuIm);
-  Vue.component(LemonMessageBasic.name, LemonMessageBasic);
   Vue.component(LemonMessageImage.name, LemonMessageImage);
   Vue.component(LemonMessageText.name, LemonMessageText);
   Vue.component(LemonMessageFile.name, LemonMessageFile);
